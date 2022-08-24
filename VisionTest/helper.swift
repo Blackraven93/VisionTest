5324842f5c81aa9cbe85b2d4dc90e0849c769e59
Blackraven93 <reblackraven@gmail.com>
2022-08-25 04:11:24 +0900

    Feat: extension CGImagePropertyOrientation

--- VisionTest/helper.swift
+++ VisionTest/helper.swift
@@ -38,3 +38,20 @@ extension UIViewController {
         return CGRect(x: scaledImageX, y: scaledImageY, width: scaledImageWidth, height: scaledImageHeight)
     }
 }
+
+extension CGImagePropertyOrientation {
+    init(_ orientation: UIImage.Orientation) {
+        switch orientation {
+        case .up: self = .up
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
+        default:
+            self = .up
+        }
+    }
+}
