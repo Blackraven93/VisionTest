225b4ffe3f5ebbea6e3d799a297b3311e7bf2bf3
Blackraven93 <reblackraven@gmail.com>
2022-08-25 04:04:49 +0900

    Feat: create UIViewController helper function about image scale

--- VisionTest/helper.swift
+++ VisionTest/helper.swift
@@ -18,4 +18,23 @@ extension UIViewController {
         
         return pointRect
     }
+    
+    private func determineScale(cgImage: CGImage, imageViewFrame: CGRect) -> CGRect {
+        let originalWidth = CGFloat(cgImage.width)
+        let originalHeight = CGFloat(cgImage.height)
+        
+        let imageFrame = imageViewFrame
+        let widthRatio = originalWidth / imageFrame.width
+        let heightRatio = originalHeight / imageFrame.height
+        
+        let scaleRatio = max(widthRatio, heightRatio)
+        
+        let scaledImageWidth = originalWidth / scaleRatio
+        let scaledImageHeight = originalHeight / scaleRatio
+        
+        let scaledImageX = (imageFrame.width - scaledImageWidth) / 2
+        let scaledImageY = (imageFrame.height - scaledImageHeight) / 2
+        
+        return CGRect(x: scaledImageX, y: scaledImageY, width: scaledImageWidth, height: scaledImageHeight)
+    }
 }
