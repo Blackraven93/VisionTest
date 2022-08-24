780a7cd3bb228c312058c5b3a81833cb734b7298
Blackraven93 <reblackraven@gmail.com>
2022-08-25 04:35:23 +0900

    Feat: setup vision function

--- VisionTest/ViewController.swift
+++ VisionTest/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
+    var imageOrientation = CGImagePropertyOrientation(.up)
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let image = UIImage(named: "group") {
+            imageView.image = image
+            imageView.contentMode = .scaleAspectFit
+            imageOrientation = CGImagePropertyOrientation(image.imageOrientation)
+            
+            guard let cgImage = image.cgImage else { return }
+        }
+    }
+    
+    private func setupVision(image: CGImage) {
+        let faceDetectionRequest = VNDetectFaceRectanglesRequest(completionHandler: self.handleFaceDetectionRequest)
+        
+        let imageRequestHandler = VNImageRequestHandler(cgImage: image, orientation: imageOrientation, options: [:])
+        
+        do {
+            try imageRequestHandler.perform([faceDetectionRequest])
+        } catch let error as NSError {
+            print(error)
+            return 
+        }
+    }
+    
+    private func handleFaceDetectionRequest( request: VNRequest?, error: Error?) {
+        
     }
 
 
