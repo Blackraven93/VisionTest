b0af646c2e2b76b01ce2f971437b0e134ad4f9bb
Blackraven93 <reblackraven@gmail.com>
2022-08-25 03:59:08 +0900

    Feat: create UIViewController helper function about image structure

--- VisionTest/helper.swift
+++ VisionTest/helper.swift
@@ -8,5 +8,14 @@
 import UIKit
 
 extension UIViewController {
-    
+    private func convertUnitToPoint (originalImageRect: CGRect, targetRect: CGRect) -> CGRect {
+        var pointRect = targetRect
+        
+        pointRect.origin.x = originalImageRect.origin.x + (targetRect.origin.x * originalImageRect.size.width)
+        pointRect.origin.y = originalImageRect.origin.y + (1 - targetRect.origin.y - targetRect.height) * originalImageRect.size.height
+        pointRect.size.width *= originalImageRect.size.width
+        pointRect.size.height *= originalImageRect.size.height
+        
+        return pointRect
+    }
 }
